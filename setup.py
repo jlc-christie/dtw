1b3e6d0cc06b8f846301091ac1d1689850cc4ec1
James Christie <jlc_christie@live.com>
2018-09-12 14:22:08 +0100

    Architecture changes

--- setup.py
+++ setup.py
@@ -1,18 +1,59 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import io
+import os
+import sys
+from shutil import rmtree
 
+from setuptools import setup, find_packages, Command
 
-setup(name='dtw',
-      version='1.3',
-      description='Python DTW Module',
-      author='Pierre Rouanet',
-      author_email='pierre.rouanet@gmail.com',
-      url='https://github.com/pierre-rouanet/dtw',
+# Meta Data
+NAME = 'dtw'
+DESCRIPTION = 'Dynamic Time Warping (DTW) in Python'
+URL = 'https://github.com/pierre-rouanet/dtw'
+EMAIL = 'pierre.rouanet@gmail.com'
+AUTHOR = 'Pierre Rouanet'
+# REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '1.4'
+
+REQUIRED = [
+        'numpy',
+        'scipy',
+    ]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+long_description = None
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+about['__version__'] = VERSION
+
+setup(name=NAME,
+      version=about['__version__'],
+      description=DESCRIPTION,
+      author=AUTHOR,
+      author_email=EMAIL,
+      url=URL,
       license='GNU GENERAL PUBLIC LICENSE Version 3',
 
-      install_requires=['numpy', 'scipy'],
+      install_requires=REQUIRED,
       setup_requires=['setuptools_git >= 0.3', ],
+      classifiers=[
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+        ],
 
       py_modules=['dtw'],
       test_suite='tests'
